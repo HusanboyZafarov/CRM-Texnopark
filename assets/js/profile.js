df777e2dc5030889c597d21f169c075d4ae3f7a5
Husanboy Zafarov <zhzr1222@gmail.com>
2023-08-29 00:27:40 +0500

    updating

--- assets/js/profile.js
+++ assets/js/profile.js
@@ -1,7 +1,9 @@
 let currency_incr = document.querySelectorAll(".header-currency_incr"),
     currency_decr = document.querySelectorAll(".header-currency_decr"),
     currency_first = document.querySelector(".header-currency_first"),
-    currency_second = document.querySelector(".header-currency_second")
+    currency_second = document.querySelector(".header-currency_second"),
+    currency_usd = document.querySelector(".header-currency_usd"),
+    currency_uzs = document.querySelector(".header-currency_uzs")
 
 let rub = 0
 let usd = 0
@@ -24,12 +26,15 @@ currency_incr.forEach(incr => {
 
     incr.addEventListener("click", () => {
         if (incr.classList[1] == "uz") {
-
             input.value = Number(Number(input.value) + 1000).toFixed(2)
             currency_first.value = Number(input.value / uzs).toFixed(2)
+            currency_uzs.textContent = input.value
+            currency_usd.textContent = currency_first.value
         } else {
             input.value = Number(Number(input.value) + 1).toFixed(2)
             currency_second.value = Number(input.value * uzs).toFixed(2)
+            currency_uzs.textContent = currency_second.value
+            currency_usd.textContent = input.value
         }
     })
 });
@@ -43,12 +48,16 @@ currency_decr.forEach(decr => {
                 input.value = Number(Number(input.value) - 1000).toFixed(2)
                 currency_first.value = Number(input.value / uzs).toFixed(2)
             }
-            input.value <= 1000 ? input.value = 0 : input.value;
-            input.value <= 1000 ? currency_first.value = 0 : currency_first.value;
+            input.value <= 1000 ? input.value = "0.00" : input.value;
+            input.value <= 1000 ? currency_first.value = "0.00" : currency_first.value;
+            currency_uzs.textContent = input.value
+            currency_usd.textContent = currency_first.value
         } else {
             if (input.value >= 1) {
                 input.value = Number(Number(input.value) - 1).toFixed(2)
                 currency_second.value = Number(input.value * uzs).toFixed(2)
+                currency_uzs.textContent = currency_second.value
+                currency_usd.textContent = input.value
             }
         }
     })
@@ -57,11 +66,22 @@ currency_decr.forEach(decr => {
 currency_first.addEventListener("keyup", (e) => {
     if (e.key != "-" || e.key != "+") {
         currency_second.value = Number(currency_first.value * uzs).toFixed(2)
+        currency_uzs.textContent = currency_second.value
+        currency_usd.textContent = currency_first.value
     }
 })
 
 currency_second.addEventListener("keyup", (e) => {
     if (e.key != "-" || e.key != "+") {
         currency_first.value = Number(currency_second.value / uzs).toFixed(2)
+        currency_uzs.textContent = currency_second.value
+        currency_usd.textContent = currency_first.value
     }
+})
+
+let currency_changer = document.querySelector(".header-currency_changer")
+currency_changer.addEventListener("click", () => {
+    currency_changer.classList.toggle("changed")
+    prev = currency_changer.previousElementSibling.classList.toggle("changed")
+    next = currency_changer.nextElementSibling.classList.toggle("changed")
 })
