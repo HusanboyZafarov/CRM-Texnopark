565a48a9f70ddb674eeb90440851a6cb7ae1a47f
Husanboy Zafarov <zhzr1222@gmail.com>
2023-09-17 22:04:57 +0500

    completing stats

---
+++ assets/js/stats.js
@@ -0,0 +1,14 @@
+var start1 = new Datepicker('#stats-form_datetime_start1', {});
+var end1 = new Datepicker('#stats-form_datetime_end1', {});
+var start2 = new Datepicker('#stats-form_datetime_start2', {});
+var end2 = new Datepicker('#stats-form_datetime_end2', {});
+
+var start3 = new Datepicker('#stats-form_datetime_start3', {});
+var end3 = new Datepicker('#stats-form_datetime_end3', {});
+var start4 = new Datepicker('#stats-form_datetime_start4', {});
+var end4 = new Datepicker('#stats-form_datetime_end4', {});
+
+window.addEventListener("load", () => {
+    let chart2 = document.querySelector("#pie-chart2")
+    chart2.style.width = `100%`
+})
