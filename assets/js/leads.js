d3b1e7bc2a8d9eed763c262003e879f641a79b30
Husanboy Zafarov <zhzr1222@gmail.com>
2023-09-18 21:18:39 +0500

    updating

--- assets/js/leads.js
+++ assets/js/leads.js
@@ -10,3 +10,9 @@ let form_download_links = document.querySelector(".leads-form_download_links")
 form_download_opener.addEventListener("click", () => {
     form_download_links.getBoundingClientRect().height ? form_download_links.style.maxHeight = 0 : form_download_links.style.maxHeight = `${form_download_links.scrollHeight}px`
 })
+
+let filter_opener = document.querySelector(".leads-filter_opener")
+filter_opener.addEventListener("click", () => {
+    filter_opener.classList.toggle("active")
+    filter_opener.parentElement.getBoundingClientRect().width == 74 ? filter_opener.parentElement.style.maxWidth = `100%` : filter_opener.parentElement.style.maxWidth = `74px`
+})
