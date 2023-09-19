815fe7aae43bcd646a83f473c0ff8781995bbc51
Husanboy Zafarov <115022906+HusanboyZafarov@users.noreply.github.com>
2023-09-19 13:37:01 +0300

    updating

--- assets/js/leads.js
+++ assets/js/leads.js
@@ -25,3 +25,33 @@ list_subcat_dropdown.forEach(dropdown => {
         list.getBoundingClientRect().height ? list.style.maxHeight = 0 : list.style.maxHeight = `${list.scrollHeight}px`
     })
 });
+
+let block_properer = document.querySelectorAll(".leads-block_properer")
+
+block_properer.forEach(changer => {
+    changer.addEventListener("click", () => {
+        changer.parentElement.parentElement.classList.toggle("changed")
+        changer.firstElementChild.classList.toggle("fa-bars")
+        changer.firstElementChild.classList.toggle("fa-grid-2")
+    })
+});
+
+let adding_form_checkbox_wrapper = document.querySelector(".leads-adding_form_checkbox_wrapper")
+let adding_form = document.querySelector(".leads-adding_form")
+adding_form_checkbox_wrapper.addEventListener("click", () => {
+    document.querySelector("#leads-adding_form_checkbox").checked ? adding_form_checkbox_wrapper.nextElementSibling.classList.remove("noned") : adding_form_checkbox_wrapper.nextElementSibling.classList.add("noned")
+})
+
+closer.addEventListener('click', () => {
+    closer.classList.remove("opened")
+    adding_form.classList.remove("opened")
+})
+let adding_form_user = document.querySelector(".leads-adding_form_user")
+let adding_form_cat = document.querySelector(".leads-adding_form_cat")
+let adding_form_opener = document.querySelectorAll(".adding_form_opener")
+adding_form_opener.forEach(element => {
+    element.addEventListener("click", () => {
+        let el = document.querySelector(`.leads-adding_form_${}`)
+        element.classList.value.split(" ")[2] == ""
+    })
+});
